b1034b2e2c93b6075d7157f3d8c44ec4ed351db6
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-05-22 13:07:07 +0200

    minor style fix

--- src/event-slot-set.lisp
+++ src/event-slot-set.lisp
@@ -45,13 +45,14 @@
 
 (defmethod propagate ((event slot-set))
   (dolist (observable (observables-of event))
+
     ;; To callbacks that should be called for "any slot" of the object in question.
     (with-callbacks (observable :slot-name t)
       (if (typep observer 'view-base) ;; TODO: This could be a method (source target event).
           (when-commit () (funcall callback event))
           (funcall callback event)))
     
-    ;; To callbacks that should be called for only the current slot in question.
+    ;; To callbacks that should be called for only the specific slot in question.
     (with-callbacks (observable :slot-name (slot-name-of event))
       (if (typep observer 'view-base) ;; TODO: This could be a method (source target event).
           (when-commit () (funcall callback event))
