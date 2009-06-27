bba684ca2f3d720cca500c676f8b01b3c842f816
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-27 17:05:45 +0200

    CELL-OF and FORMULA-OF will not generate warnings by default now.

--- src/meta-class.lisp
+++ src/meta-class.lisp
@@ -261,23 +261,31 @@ See their doc-strings for info."))
        value))))
 
 
-(defmacro cell-of (arg)
+(defmacro cell-of (arg &key warn-p error-p)
   "This is used to extract a CELL instance from \"something\". This tends to
 mean CELL instances stored in CLOS slots (:METACLASS MVC-CLASS)."
   (with-gensyms (result)
     `(let* ((*get-cell-p* t)
             (,result ,arg))
-       (unless (typep ,result 'cell)
-         (warn "SW-MVC:CELL-OF: Returning something not a CELL; ~S" ,result))
-       ,result)))
+       (prog1 ,result
+         ,(when warn-p
+           `(unless (typep ,result 'cell)
+              (warn "~A, ~A returning something not a CELL; ~S" =lex-function-name= 'cell-of ,result)))
+         ,(when error-p
+           `(unless (typep ,result 'cell)
+              (error "~A, ~A returning something not a CELL; ~S" =lex-function-name= 'cell-of ,result)))))))
 
 
-(defmacro formula-of (arg)
+(defmacro formula-of (arg &key warn-p error-p)
   "This is used to extract a FORMULA instance from \"something\". This tends to
 mean FORMULA instances stored in CLOS slots (:METACLASS MVC-CLASS)."
   (with-gensyms (result)
     `(let* ((*get-formula-p* t)
             (,result ,arg))
-       (unless (typep ,result 'formula)
-         (warn "SW-MVC:FORMULA-OF: Returning something not a FORMULA; ~A" ,result))
-       ,result)))
+       (prog1 ,result
+         ,(when warn-p
+           `(unless (typep ,result 'formula)
+              (warn "~A, ~A returning something not a FORMULA; ~A" =lex-function-name= 'formula-of ,result)))
+         ,(when error-p
+           `(unless (typep ,result 'formula)
+              (error "~A, ~A returning something not a FORMULA; ~A" =lex-function-name= 'formula-of ,result)))))))
