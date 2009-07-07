ef942e7c2836d9bec171c216ff86885d35156983
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-07 23:37:31 +0200

    Seemed like a good idea, but let's not have the extra noise there by default.

--- tests/input-translation-simple.lisp
+++ tests/input-translation-simple.lisp
@@ -3,14 +3,11 @@
 (in-package #:sw-mvc)
 
 
-(terpri)
-
-
 (let* ((input #~"0")
        (output (mk-validator #'evenp (mk-number-parser input)))
        (confirm 0)
        (update-ui #λ(progn
-                      (format t "Update UI based on new OUTPUT: ~A~%" ~output)
+                      #|(format t "Update UI based on new OUTPUT: ~A~%" ~output)|#
                       (assert (and (= confirm ~output)
                                    (evenp confirm))))))
   (declare (ignorable update-ui))
@@ -22,14 +19,11 @@
   ~output)
 
 
-(terpri)
-
-
 (let* ((input #~"1")
        (output (mk-validator #'oddp (mk-number-parser input)))
        (confirm 1)
        (update-ui #λ(progn
-                      (format t "Update UI based on new OUTPUT: ~A~%" ~output)
+                      #|(format t "Update UI based on new OUTPUT: ~A~%" ~output)|#
                       (assert (and (= confirm ~output)
                                    (oddp confirm))))))
   (declare (ignorable update-ui))
@@ -39,6 +33,3 @@
                  ~input (format nil "~D" i)
                  ~input "junk")))
   ~output)
-
-
-(terpri)
