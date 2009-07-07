8e79c133ed1c47652909ea078b6bb0ebcb2e44b3
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-07 23:36:58 +0200

    Add a second clos test.

--- tests/clos-simple.lisp
+++ tests/clos-simple.lisp
@@ -3,27 +3,46 @@
 (in-package #:sw-mvc)
 
 
-(eval-now (setf (find-class 'clos-simple-test nil) nil))
-
-
-(defclass clos-simple-test (self-ref)
+(defclass clos-simple-test-1 (self-ref)
   ((x :initform #λ2)
    (square :initform ↑#λ(* ¤x ¤x)))
   (:metaclass mvc-class))
 
-(with-object (make-instance 'clos-simple-test)
+(with-object (make-instance 'clos-simple-test-1)
   (assert (equalp (list 2 4) (list ¤x ¤square)))
   (incf ¤x)
   (assert (equalp (list 3 9) (list ¤x ¤square)))
   (catch :abort
     (with-sync ()
       (incf ¤x)
+      (assert (equalp (list 4 16) (list ¤x ¤square)))
       (throw :abort nil)))
-  ;; Back to what it was @ second step.
   (assert (equalp (list 3 9) (list ¤x ¤square))))
 
 
-(eval-now (setf (find-class 'clos-simple-test nil) nil)
-          (unintern 'clos-simple-test)
+;;; Same, but using the custom slot metaclass here.
+
+(defclass clos-simple-test-2 (self-ref)
+  ((x :cell-p t :initform 2)
+   (square :cell-p t :initform ↑λλ(* ¤x ¤x)))
+  (:metaclass mvc-class))
+
+(with-object (make-instance 'clos-simple-test-2)
+  (assert (equalp (list 2 4) (list ¤x ¤square)))
+  (incf ¤x)
+  (assert (equalp (list 3 9) (list ¤x ¤square)))
+  (catch :abort
+    (with-sync ()
+      (incf ¤x)
+      (assert (equalp (list 4 16) (list ¤x ¤square)))
+      (throw :abort nil)))
+  (assert (equalp (list 3 9) (list ¤x ¤square))))
+
+
+
+(eval-now (nilf (find-class 'clos-simple-test-1 nil)
+                (find-class 'clos-simple-test-2 nil))
+          (unintern 'clos-simple-test-1)
+          (unintern 'clos-simple-test-2)
           (unintern 'x)
           (unintern 'square))
