3493a94df7ccd70957e311ed37e289ce171c54e9
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-09 01:40:48 +0200

    ...

--- tests/set-formula-@-transaction.lisp
+++ tests/set-formula-@-transaction.lisp
@@ -3,11 +3,12 @@
 (in-package #:sw-mvc)
 
 
-(let* ((i #~2)
+(let* ((i #λ2)
        (x #λ(+ 40 ~i)))
   (catch :abort
     (with-sync ()
-      (setf (formula-of ~x) (lambda () :fail))
+      (setf (formula-of x) (lambda () 1))
       (throw :abort nil)))
+
   (incf ~i) ;; The transaction was aborted, so we're back to the old formula.
   (assert (= 43 ~x)))
