7272e6a4864a69f1eda6f2ed4d52c43520b5b9f4
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-09 05:00:17 +0200

    HANDLE :AROUND deleted for now; we don't do the *EVENT-STACK* thing anymore.

--- src/event.lisp
+++ src/event.lisp
@@ -14,12 +14,6 @@
 This \"handles\" or \"executes\" the EVENT."))
 
 
-(defmethod handle :around (event)
-  (let ((*event-stack* (cons event *event-stack*)))
-    (with-simple-restart (abort-mvc-event "SW-MVC: Abort the SW-MVC event; ~A" event)
-      (call-next-method))))
-
-
 (defgeneric observables-of (event)
   (:method-combination append)
   (:documentation "
