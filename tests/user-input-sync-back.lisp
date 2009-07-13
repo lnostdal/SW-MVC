ab9afd9c399062ce2dc1efb1bee3543260973ac8
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-13 16:51:52 +0200

    A "sync-back" can be expressed directly now as the detection of circular propagation has changed.

--- tests/user-input-sync-back.lisp
+++ tests/user-input-sync-back.lisp
@@ -24,15 +24,12 @@ the View (USER-INPUT) to update. At the same time, stuff should not get stuck pr
 |#
 
 
-(let* ((user-input #λ0)
-       (x #λ0)
+(let* ((x #λ0)
+       (user-input #λ~x) ;; X -> USER-INPUT (sync-back).
        (square #λ(* ~x ~x)))
 
   ;; USER-INPUT -> X.
   (forward-cell (mk-validator #'evenp (mk-number-parser user-input)) x)
-
-  ;; X -> USER-INPUT (sync-back).
-  (forward-cell x user-input)
 
   (setf ~user-input "2")
   (assert (string= "2" ~user-input))
