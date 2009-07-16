f860d1af27d8e86c6ca9d4297621cc936f78c741
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-16 18:18:43 +0200

    Some user-definable restrictions wrt. lenght of input for MK-NUMBER-PARSER and MK-INTEGER-PARSER was added.

--- src/input-translators.lisp
+++ src/input-translators.lisp
@@ -4,11 +4,13 @@
 
 
 (declaim (ftype (function (cell function &key
-                                (:feedback-cell (or null cell))
+                                (:feedback-cell t)
                                 (:equal-p-fn (or null function)))
                           (values cell &optional))
                 mk-input-handler))
 (defun mk-input-handler (input-cell translator-fn &key feedback-cell equal-p-fn)
+  (when (eq t feedback-cell)
+    (setf feedback-cell (feedback-event-of input-cell)))
   (letp1 ((cell λi(let ((input ~input-cell)
                         (old-value =cell-old-value=))
                     (handler-case
@@ -39,32 +41,38 @@
                     :feedback-cell feedback-cell))
 
 
-(declaim (ftype (function (cell &optional cell) (values cell &optional))
+(declaim (ftype (function (cell &key (:feedback-cell t) (:max-input-length fixnum))
+                          (values cell &optional))
                 mk-integer-parser))
-(defun mk-integer-parser (input-cell &optional feedback-cell)
+(defun mk-integer-parser (input-cell &key (feedback-cell t) (max-input-length 50))
   "Returns a CELL that'll represent the result of sending the content of
 INPUT-CELL through AMX:PARSE-INTEGER."
   (mk-input-handler input-cell
                     (lambda (input)
                       (typecase input
-                        (string (parse-integer input))
+                        (string
+                         (assert (> max-input-length (length input)) nil
+                                 "MK-INTEGER-PARSER: Input too long to parse.")
+                         (parse-integer input))
                         (integer input)
                         (t (error "MK-INTEGER-PARSER (lambda): Don't know what to do with input: ~S~%" input))))
                     :equal-p-fn #'=
                     :feedback-cell feedback-cell))
 
 
-(declaim (ftype (function (cell &optional t) (values cell &optional))
+(declaim (ftype (function (cell &key (:feedback-cell t) (:max-input-length fixnum))
+                          (values cell &optional))
                 mk-number-parser))
-(defun mk-number-parser (input-cell &optional feedback-cell)
+(defun mk-number-parser (input-cell &key (feedback-cell t) (max-input-length 50))
   "Returns a CELL that'll represent the result of sending the content of
 INPUT-CELL through AMX:PARSE-NUMBER."
-  (when (eq t feedback-cell)
-    (setf feedback-cell (feedback-event-of input-cell)))
   (mk-input-handler input-cell
                     (lambda (input)
                       (typecase input
-                        (string (parse-number input))
+                        (string
+                         (assert (> max-input-length (length input)) nil
+                                 "MK-NUMBER-PARSER: Input too long to parse.")
+                         (parse-number input))
                         (number input)
                         (t (error "MK-NUMBER-PARSER (lambda): Don't know what to do with input: ~S~%" input))))
                     :equal-p-fn #'=
