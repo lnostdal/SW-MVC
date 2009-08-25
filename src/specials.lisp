745505b4606e3f58910b5068a64a9a29bb475327
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-08-25 13:14:32 +0200

    Add always-boundp declarations to our dynamic variables.

--- src/specials.lisp
+++ src/specials.lisp
@@ -10,12 +10,15 @@
 
 (define-variable *get-cell-p*
     :value nil
-    :type (member t nil))
+    :type (member t nil)
+    :always-boundp t)
 
 (define-variable *source-cells*
     :value nil
-    :type list)
+    :type list
+    :always-boundp t)
 
 (define-variable *target-cell*
     :value nil
-    :type (or null cell))
+    :type (or null cell)
+    :always-boundp t)
