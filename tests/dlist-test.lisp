0e5cff1ff8182b1f1364c504f6e58626d908760b
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-08-27 13:25:09 +0200

    Make the test for DLIST a bit more interesting.

--- tests/dlist-test.lisp
+++ tests/dlist-test.lisp
@@ -3,10 +3,14 @@
 (in-package #:sw-mvc)
 
 
-(assert (sequence-of-length-p (let ((c (dlist #λ0 #λ1)))
+(assert (sequence-of-length-p (let ((c (dlist #λ0 #λ1))
+                                    (new #λ2))
+                                (assert (not (node-of new)))
                                 (catch :fail
                                   (with-sync ()
-                                    (insert #λ2 :in c)
+                                    (insert new :in c)
+                                    (assert (node-of new))
                                     (throw :fail nil)))
+                                (assert (not (node-of new)))
                                 ~c)
                               2))
