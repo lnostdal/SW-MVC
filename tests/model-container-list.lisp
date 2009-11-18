300b680cfa4b5fb78f4722011c03497ff00d3970
Lars Rune Nøstdal <lnostdal@blackbox.(none)>
2009-11-18 19:10:33 +0100

    ..I suspect the method was getting removed before it was actually called here(?) in some cases so I've shuffled this stuff around a bit..

--- tests/model-container-list.lisp
+++ tests/model-container-list.lisp
@@ -4,25 +4,26 @@
 (in-readtable sw-mvc)
 
 
-(defclass test-view (view-base)
-  ())
+(flet ((test ()
+         (defclass test-view (view-base) ())
+
+         (let ((worksp nil))
+
+           (defmethod (setf model-of) ((container dlist) (view test-view))
+             (with-formula view
+               (when-let (event (event-of container))
+                 (tf worksp))))
+
+           (let* ((model (dlist))
+                  (view (make-instance 'test-view :model model)))
+             (insert λV42 :in model)
+             (assert worksp)
+             view))
 
 
-(let ((worksp nil))
-
-  (defmethod (setf model-of) ((container dlist) (view test-view))
-    (with-formula view
-      (when-let (event (event-of container))
-        (tf worksp))))
-
-  (let* ((model (dlist))
-         (view (make-instance 'test-view :model model)))
-    (insert #λ42 :in model)
-    (assert worksp)
-    view))
-
-
-(remove-method #'(setf model-of)
-               (find-method #'(setf model-of) nil (mapcar #'find-class '(dlist test-view))))
-(setf (find-class 'test-view nil) nil)
-(unintern 'test-view)
+         ;; TODO: Ho-hum. I think this actually manages to race with the call to INSERT above.
+         (remove-method #'(setf model-of)
+                        (find-method #'(setf model-of) nil (mapcar #'find-class '(dlist test-view))))
+         (setf (find-class 'test-view nil) nil)
+         (unintern 'test-view)))
+  (test))
