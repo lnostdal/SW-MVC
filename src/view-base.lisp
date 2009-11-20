3f3fc61bcb385f8611dc102dbc3bb43b5c0fa164
Lars Rune Nøstdal <lnostdal@blackbox.(none)>
2009-11-20 13:33:36 +0100

    VIEW-BASE: I can't think of a single widget that has a use for the VIEW-CONSTRUCTOR-FN slot at the moment, and if it did it could add it itself.

--- src/view-base.lisp
+++ src/view-base.lisp
@@ -10,24 +10,13 @@
           :initform λVnil)
 
    ;; Dataflow: MODEL -> MODEL-OBSERVERS => VIEW-BASE (some widget in SW).
-   ;; This slot is set by the :AROUND (SETF MODEL-OF) method.
    (model-observers :reader model-observers-of
                     :type list
                     :initform nil)
 
    ;; [SIGNATURE (context-view . model) -> VIEW]
    (views-in-context :type hash-table
-                     :initform (make-hash-table :test #'equal :weakness :value :synchronized t))
-
-   (view-constructor-fn :accessor view-constructor-fn-of :initarg :view-constructor-fn
-                        :type function
-                        :initform (lambda (context-view model &key)
-                                    (declare (ignore model))
-                                    (error "SW-MVC: The VIEW-CONSTRUCTOR-FN slot in ~A has not been assigned."
-                                           context-view))
-                        :documentation "
-Function with lambda-list (CONTEXT-VIEW MODEL &REST ARGS). Must return an
-object which is a sub-type of VIEW-BASE.")))
+                     :initform (make-hash-table :test #'equal :weakness :value :synchronized t))))
 
 
 (defmethod initialize-instance :before ((view view-base) &key model)
@@ -67,16 +56,6 @@ object which is a sub-type of VIEW-BASE.")))
 (add-deref-type 'view-base
                 :get-expansion (λ (arg-sym) `(model-of ,arg-sym))
                 :set-expansion t)
-
-
-(defmethod view-constructor ((context-view view-base) model)
-  "This is called to construct a new View based on MODEL in context of
-CONTEXT-VIEW (probably a container). The default method calls the function held
-in the slot VIEW-CONSTRUCTOR-FN in CONTEXT-VIEW if there is a function there."
-  (if-let ((view-constructor-fn (view-constructor-fn-of context-view)))
-    (funcall (the function view-constructor-fn) context-view model)
-    (error "No suitable VIEW-CONSTRUCTOR method found for ~A and ~A,
-and VIEW-CONSTRUCTOR-FN in ~A was NIL." context-view model context-view)))
 
 
 (defun view-in-context-of (context-view model &optional create-if-not-found-p)
